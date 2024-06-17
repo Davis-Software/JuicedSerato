22c3786a8335de9a7b7d6fc6b0073f40417edf68
Davis-Software <davissoftware6@gmail.com>
2024-06-17 19:27:41 +0200

    implemented actual conversion for copy operation

--- juicy_serato/cli/cli_copy.py
+++ juicy_serato/cli/cli_copy.py
@@ -1,14 +1,15 @@
 import click
 
 from .. import cli_base
+from .._juicer import SeratoJuicer
 
 
 @cli_base.command()
-@click.option("-r", "--recursive", is_flag=True, default=False, help="Recursively search for files")
-@click.argument("src", type=click.Path(exists=True, writable=True, readable=True, resolve_path=True),
-                nargs=-1, required=True)
 @click.argument("dst", type=click.Path(writable=True, resolve_path=True, file_okay=False), required=True)
 @click.pass_context
-def copy(ctx, recursive, src, dst):
-    print(recursive, src, dst)
-    pass
+def copy(ctx, dst):
+    database = ctx.obj["DATABASE"]
+
+    with SeratoJuicer(database) as juicer:
+        juicer.copy(dst)
+        juicer.inject()
--- juicy_serato/cli/cli_inject.py
+++ juicy_serato/cli/cli_inject.py
@@ -4,10 +4,6 @@ from .. import cli_base
 
 
 @cli_base.command()
-@click.option("-r", "--recursive", is_flag=True, default=False, help="Recursively search for files")
-@click.argument("src", type=click.Path(exists=True, writable=True, readable=True, resolve_path=True),
-                nargs=-1, required=True)
 @click.pass_context
-def inject(ctx, recursive, src):
-    print(recursive, src)
+def inject(ctx):
     pass
