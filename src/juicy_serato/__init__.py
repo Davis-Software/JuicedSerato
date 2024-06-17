b0dd25ca7d8080a6fc1c1a51ef996fc2b4daf20f
Davis-Software <davissoftware6@gmail.com>
2024-06-17 16:34:13 +0200

    added build utils

---
+++ src/juicy_serato/__init__.py
@@ -0,0 +1,24 @@
+import click
+
+
+def start_gui_callback(ctx, _param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    raise NotImplementedError("The GUI is not yet implemented")
+
+
+@click.group()
+@click.option("-b", "--database", required=True, help="DJuiced Database",
+              type=click.Path(exists=True, dir_okay=False, writable=False, readable=True))
+@click.option("-G", "--gui", is_flag=True, default=False, help="Launch JuicedSerato GUI",
+              is_eager=True, expose_value=False, callback=start_gui_callback)
+@click.pass_context
+def cli_base(ctx, database):
+    ctx.ensure_object(dict)
+    ctx.obj["DATABASE"] = database
+
+
+if __name__ == "__main__":
+    from .cli import cli_inject
+
+    cli_base()
---
+++ src/juicy_serato/cli/cli_inject.py
@@ -0,0 +1,6 @@
+from ..__init__ import cli_base
+
+
+@cli_base.command()
+def inject():
+    pass
